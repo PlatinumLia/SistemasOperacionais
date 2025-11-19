ed766a3e9cb9ae61cdf032e6841598f4192ea432
Lia <134401063+PlatinumLia@users.noreply.github.com>
2025-11-19 20:10:54 -0300

    Add files via upload

--- Aula 02 - Exercícios Bash/08.sh
+++ Aula 02 - Exercícios Bash/08.sh
@@ -2,3 +2,17 @@
 
 echo "--------------------"
 read -p "Preço do produto: " preco
+clear
+
+descontoD=0.1
+descontoC=0.05
+
+if ((preco > 100)); then
+    desconto=$(echo "$descontoD * $preco" | bc)
+    precoDesconto=$(echo "$preco - $desconto" | bc)
+    echo "Preço com desconto de 10%: $precoDesconto"
+else
+    desconto=$(echo "$descontoC * $preco" | bc)
+    precoDesconto=$(echo "$preco - $desconto" | bc)
+    echo "Preço com 5% de desconto: $precoDesconto"
+fi #encerra o if
--- Aula 02 - Exercícios Bash/09.sh
+++ Aula 02 - Exercícios Bash/09.sh
@@ -1,1 +1,18 @@
 # Peça o peso (kg) e altura (m) do usuário e calcule o IMC
+
+echo "--------------------"
+read -p "Seu peso (kg): " peso
+read -p "Sua altura (m): " altura
+clear
+
+imc=$(echo "$peso / ($altura * 2)" | bc)
+
+if [ $(echo "$imc < 18.5" | bc -l) -eq 1 ]; then
+    echo "Abaixo do peso."
+elif [ $(echo "$imc >= 18.5" | bc -l) -eq 1 ] && [ $(echo "$imc <= 24.9" | bc -l) -eq 1 ]; then
+    echo "Peso normal."
+elif [ $(echo "$imc >= 25" | bc -l) -eq 1 ] && [ $(echo "$imc < 29.9" | bc -l) -eq 1 ]; then
+    echo "Sobrepeso."
+else
+    echo "Obesidade"
+fi #finaliza o if
